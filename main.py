27fbe5d862eb8cb263b52f1b99dc6af5c472723f
Bella Tom <btom0318@gmail.com>
2023-06-17 13:28:35 -1000

    Create main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 def main():
+   
+
 
     ##############################
     # make your code below
     # print('Hello World')
     ##############################
-
+    print ('Hello World')
+    print ('CS7: Python Programming')
 
 if __name__ == '__main__':
     main()
+    
